767a8d575c1d757fe1815d617a758df85a8afc1a
budzikovy <115116202+budzikovy@users.noreply.github.com>
2024-12-02 16:46:51 +0100

    Add files via upload

---
+++ src/test/java/com/budzikovy/medical_clinic_proxy/MedicalClinicProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budzikovy.medical_clinic_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
